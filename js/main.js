4cb9cf3406db3c4b3272587fab02ce015a3a0796
Alex <alex.mahinko@gmail.com>
2017-05-01 19:52:28 -0400

    Functions implemented with proper event handlers

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
+/// Monitors light state
+var lightOn = true;
+
+/// Switch on/off button
+function buttonSwitch () {
+  lightOn = !lightOn;
+  if (lightOn == false) {
+    console.log("running light off");
+    $('button').attr('class', 'switch off');
+    $('body').attr('class', 'dark');
+    $('.status').html('And now, the darkness holds dominion. Black as death.');
+
+  }
+
+  else {
+    console.log("running light on");
+    $('button').attr('class', 'switch on');
+    $('body').attr('class', 'light');
+    $('.status').html('The match is struck! A blazing star is born!');
+
+  }
+}
 
 
+/// Button event handler
+$('button').click(buttonSwitch);
